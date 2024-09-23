70a46e13aad5097875cc5fd058fd15aa235b51c0
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-09-23 13:09:59 +0200

    Adding tailwind to the project and shadcn a ui framework

--- Frontend/assementtool/src/UploadPDF.jsx
+++ Frontend/assementtool/src/UploadPDF.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './Upload.css'; // Import the CSS file
 
 const UploadPDF = () => {
   const [file, setFile] = useState(null);
@@ -45,29 +44,29 @@ const UploadPDF = () => {
   };
 
   return (
-    <>
-      <h1>Bloom Assessment Tool Checker</h1>
-      <div className='container'>
-        <div className='upload-card'>
-          <h2>Upload PDF</h2>
-          <form onSubmit={handleSubmit}>
-            <label className="custom-file-upload">
-              <input type="file" accept="application/pdf" onChange={handleFileChange} />
-              Choose File
-            </label>
-            <button type="submit" disabled={loading}>Upload</button>
-          </form>
+    <div className="h-screen flex items-center justify-center flex-col bg-gray-900">
+        <h1 className="text-6xl text-cyan-600 text-center mb-4">Bloom Assessment Tool Checker</h1>
+      <div className="border border-gray-900 rounded-lg p-6 bg-gray-50 shadow-lg w-1/4 bg-slate-800">
+        <h2 className="text-cyan-600 text-center text-2xl mb-4">Upload PDF</h2>
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <label className="custom-file-upload border border-gray-300 rounded-md bg-white p-2 text-center cursor-pointer hover:bg-gray-200 transition duration-300 mb-2">
+            <input type="file" accept="application/pdf" onChange={handleFileChange} className="hidden" />
+            Choose File
+          </label>
+          <button type="submit" disabled={loading} className="bg-cyan-400 text-neutral-50 py-2 rounded-md hover:bg-cyan-500 transition duration-300">
+            Upload
+          </button>
+        </form>
 
-          {loading && (
-            <div className="spinner-container">
-              <div className="spinner"></div>
-            </div>
-          )}
+        {loading && (
+          <div className="flex justify-center items-center h-6 p-6">
+            <div className="border-4 border-gray-200 border-l-blue-500 rounded-full w-10 h-10 animate-spin"></div>
+          </div>
+        )}
 
-          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-        </div>
+        {errorMessage && <p className="text-red-500 text-center">{errorMessage}</p>}
       </div>
-    </>
+    </div>
   );
 };
 
